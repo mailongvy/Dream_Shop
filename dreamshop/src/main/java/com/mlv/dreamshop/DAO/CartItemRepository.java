03f47cb8453d4982e6873caab541619be7a8fa46
mailongvy <mlongvy0708@gmail.com>
2025-04-07 18:38:43 +0700

    latest update

--- dreamshop/src/main/java/com/mlv/dreamshop/DAO/CartItemRepository.java
+++ dreamshop/src/main/java/com/mlv/dreamshop/DAO/CartItemRepository.java
@@ -1,9 +1,11 @@
 package com.mlv.dreamshop.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mlv.dreamshop.Model.CartItem;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     void deleteAllByCartId(Long id);
--- dreamshop/src/main/java/com/mlv/dreamshop/DAO/CartRepository.java
+++ dreamshop/src/main/java/com/mlv/dreamshop/DAO/CartRepository.java
@@ -1,9 +1,11 @@
 package com.mlv.dreamshop.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mlv.dreamshop.Model.Cart;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
