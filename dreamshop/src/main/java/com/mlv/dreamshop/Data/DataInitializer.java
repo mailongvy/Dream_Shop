c19d7a18609ef4e8dbdc7a899b6a5bbfcb17dff6
mailongvy <mlongvy0708@gmail.com>
2025-04-23 15:27:27 +0700

    data initializer create 5 user samples for the database

---
+++ dreamshop/src/main/java/com/mlv/dreamshop/Data/DataInitializer.java
@@ -0,0 +1,47 @@
+package com.mlv.dreamshop.Data;
+
+import com.mlv.dreamshop.DAO.UserRepository;
+import com.mlv.dreamshop.Model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        createDefaultUserIfNotExist();
+    }
+
+    public void createDefaultUserIfNotExist() {
+        for (int i = 1; i <= 5; i++) {
+            String defaultEmail = "user" + i + "@gmail.com";
+            if (userRepository.existsByEmail(defaultEmail)) {
+                continue; // nếu tìm thấy email tượng tự sẽ bỏ qua
+            }
+            User user = new User();
+
+            user.setFirstName("The User");
+            user.setLastName("User" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword("123456");
+
+            userRepository.save(user);
+
+            System.out.println("Default vet user " + i + " created successfully");
+
+
+
+        }
+    }
+
+    @Override
+    public boolean supportsAsyncExecution() {
+        return ApplicationListener.super.supportsAsyncExecution();
+    }
+}
