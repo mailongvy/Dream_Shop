64d28d2235f791ad531ef701752683c54959f8fc
mailongvy <mlongvy0708@gmail.com>
2025-05-03 00:12:43 +0700

    set up for jwtutils

---
+++ dreamshop/src/main/java/com/mlv/dreamshop/security/jwt/JwtUtils.java
@@ -0,0 +1,42 @@
+package com.mlv.dreamshop.security.jwt;
+
+import com.mlv.dreamshop.security.user.ShopUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+public class JwtUtils {
+    private String jwtSecret;
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication) {
+        ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return Jwts.builder()
+                .subject(userPrincipal.getEmail())
+                .claim("id", userPrincipal.getId())
+                .claim("role", roles)
+                .issuedAt(new Date())
+                .expiration(new Date( (new Date()).getTime() + expirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    //
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+}
