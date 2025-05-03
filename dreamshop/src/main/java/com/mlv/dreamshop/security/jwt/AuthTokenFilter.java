81996d17f1d78dd2e0835e713e3ff25ec2d0fc75
mailongvy <mlongvy0708@gmail.com>
2025-05-03 14:52:00 +0700

    update for JwtUtils class and complete AuthTokenFilter class

---
+++ dreamshop/src/main/java/com/mlv/dreamshop/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,55 @@
+package com.mlv.dreamshop.security.jwt;
+
+import com.mlv.dreamshop.security.user.ShopUserDetailsService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+    private ShopUserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = parseJwt(request);
+            if (jwtUtils.validateToken(token) && StringUtils.hasText(token)) {
+                String username = jwtUtils.getUsernameFromToken(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage() + "Invalid or expired token");
+            return;
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            return headerAuth;
+        }
+
+        return null;
+    }
+}
--- dreamshop/src/main/java/com/mlv/dreamshop/security/jwt/JwtUtils.java
+++ dreamshop/src/main/java/com/mlv/dreamshop/security/jwt/JwtUtils.java
@@ -1,19 +1,24 @@
 package com.mlv.dreamshop.security.jwt;
 
 import com.mlv.dreamshop.security.user.ShopUserDetails;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtils {
+    @Value("${auth.token.jwtSecret}")
     private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
     private int expirationTime;
 
     public String generateTokenForUser(Authentication authentication) {
@@ -38,5 +43,28 @@ public class JwtUtils {
     //
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    // get username from token
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey( key() ).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+    }
+
+    // check validate token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key()).build().parseClaimsJws(token);
+            return true;
+        }
+        catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | io.jsonwebtoken.security.SecurityException | IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+
+        }
+
     }
 }
