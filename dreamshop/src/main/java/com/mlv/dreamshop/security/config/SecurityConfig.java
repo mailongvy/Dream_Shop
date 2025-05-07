c85b10bd8cef82a22a61242d3df9f3c707af33b4
mailongvy <mlongvy0708@gmail.com>
2025-05-07 09:29:28 +0700

    Update securityConfig and create authcontroller jwtresponse loginrequest

--- dreamshop/src/main/java/com/mlv/dreamshop/security/config/SecurityConfig.java
+++ dreamshop/src/main/java/com/mlv/dreamshop/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final ShopUserDetailsService userDetailsService;
     private final JwtUtils jwtUtils;
